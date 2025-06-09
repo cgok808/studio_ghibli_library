c0d749a7383b9f940c9a43533f5adea8b60a5142
cgok808 <chaylin.gokan@gmail.com>
2025-06-08 16:16:30 -1000

    updated nav's responsive design

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,28 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Logo from "../assets/logo.png";
+import { ShoppingCart } from "lucide-react";
+import { Menu } from "lucide-react";
 
-const Nav = () => {
+const Nav = ({ numberOfItems }) => {
   return (
     <nav className='bg-[#ddd4bb]'>
-      <div className='w-full max-w-7xl py-6 flex justify-between items-center mx-auto text-white'>
+      <div className='w-full flex items-center justify-between p-6 px-10 lg:px-20'>
         <Link to='' className='flex'>
           <img src={Logo} alt='' className='w-14 rounded-full' />
         </Link>
         <ul className='flex'>
-          <li className='ml-4 font-bold'>
+          <li className='ml-4 font-bold hidden sm:block'>
             <Link className='' to='/'>
               Home
             </Link>
           </li>
-          <li className='ml-4 font-bold'>
+          <li className='ml-4 font-bold hidden sm:block'>
             <Link className='' to='/films'>
               Catalogue
             </Link>
           </li>
+          <div className='sm:hidden mr-2'>
+            <Menu />
+          </div>
           <li className='ml-4'>
             <Link className='' to='/cart'>
-              Cart
+              <ShoppingCart />
             </Link>
           </li>
         </ul>
